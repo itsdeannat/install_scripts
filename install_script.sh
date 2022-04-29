0db3059068903c866bdd13a2bc1327baa0e281b4
Deanna Thompson <deanna.thompson45@outlook.com>
2022-04-29 11:54:42 -0400

    Add first version of install script

---
+++ install_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "New computer? Time to install all the things."
+
+# set prompt for packages user wants to install 
+PS3="Select the package you want to install or type 'done' to cancel: "
+packages=("pip" "python" "vim" "npm" "hugo" "eleventy")
+
+# select construct generates a numbered menu
+select package in "${packages[@]}"
+do
+    # leave the loop if the user stays 'done'
+    if [[ "$REPLY" = "done" ]]; then break; fi
+
+    # complain if no file was selected, and loop to ask again 
+    if [[ "$package" = "" ]]; then
+       echo "'$package' is not a valid choice"
+       continue
+    fi
+
+    # now install selected package
+    echo "Installing $package"
+    echo "This is a test"
+
+    continue
+done	
+echo "Enjoy your new computer!"
